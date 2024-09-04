314f47408077223b4a16e5467cda75d3a0f13046
Layheang <hak.layheang277@gmail.com>
2024-09-04 12:57:13 +0700

    design more UI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css';
 import App from './App.vue'
 
 const app = createApp(App)
 const pinia = createPinia();
 app.use(pinia)
+app.use(PrimeVue, {
+    ripple:true,
+    theme: {
+        preset: Aura,
+        options : {
+          darkModeSelector: ''
+        } 
+    }
+});
 app.mount("#app")
--- src/views/Homepage/components/headerPage.vue
+++ src/views/Homepage/components/headerPage.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";  
+import moment from "moment"; 
+import Dialog from 'primevue/dialog'; // import for primevue to use 
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import Password from "primevue/password";
+
 const fullDate = ref(new Date()); 
 const date = ref(); 
 const time = ref();
 const greeting = ref('');  
-const updateCurrentTime = () => {  
-    time.value = new Date().toLocaleTimeString();  
+const visible = ref(false); // use primevue for login and config style with tailwindcss
+let isLoading = false
+const updateCurrentTime = () => {   // function for run time that use moment liary 
+    time.value = moment(new Date()).format('hh:mm:ss A');  
 };  
 
 
 onMounted(() => { 
     greeting.value = checkTimeShift() 
-    setInterval(() => { 
-        greeting.value = checkTimeShift(); 
-    }, 60000); 
-    date.value = fullDate.value.getFullYear() + '-' + (fullDate.value.getMonth() + 1).toString().padStart(2, "0") + '-' + fullDate.value.getDate().toString().padStart(2, "0"); 
+    date.value = moment(new Date()).format('DD-MM-YYYY') // real date 
     updateCurrentTime()
     setInterval(() => { 
         updateCurrentTime()
@@ -41,24 +46,72 @@ const checkTimeShift = () => {
 <template>
         <div class=" w-[100%] flex items-center bg-[rgba(124, 147, 195,0.9)] backdrop-blur-sm">
             <div class="w-full justify-between flex p-1">
-                <div class="w-[33.33%] p-2">
-                    <headerMenu>hello</headerMenu>
-                </div>
+                <div class=" gap-4 flex justify-around items-center column-gap-2 xl:column-gap-4 text-[15px] w-[33.33%] font-bold" > 
+                        <p>{{ greeting }}</p> 
+                        <div class="w-[50%] justify-around flex ">
+                            <p >{{ date }}</p> 
+                            <p class="min-w-24">{{ time }}</p> 
+                        </div>
+                    </div>
                 <div class="flex justify-center w-[33.33%] ">
-                    <a target="_blank" href="https://www.norton-u.com/">
                         <img src="../assets/imgs/logo.png" alt="logo-Norton"
                             class="w-[200px] ">
-    
-                    </a>
                 </div>
-                    <div class=" p-2 gap-8 flex justify-around items-center column-gap-2 xl:column-gap-4 text-[15px] w-[33.33%] font-bold" > 
-                        <p>{{ greeting }}</p> 
-                        <p >{{ date }}</p> 
-                        <p class="min-w-24">{{ time }}</p> 
+                <div class="w-[33.33%] p-1  ">
+                    <div class="card flex justify-center p-2">
+                        <Button security="Login" outlined class="login-button" @click="visible = true" :loading="isLoading">
+                                <label class="text-black font-bold ">Login</label>
+                        </Button> 
+                                 
+                        <Dialog v-model:visible="visible" pt:root:class="bg-transparent !border-0 !bg-transparent" pt:mask:class="backdrop-blur-sm">
+                            <template #container="{ closeCallback }">
+                                <div class="flex flex-col px-8 py-8 gap-6 rounded-2xl h-[450px] bg-[#CDF0EA] backdrop-blur-md" 
+                                     @keydown.space.prevent>
+                                     <span class="block mx-auto font-bold text-xl">Login</span>
+                                        <div class="inline-flex flex-col gap-2"  >
+                                            <label for="Email" class="text-primary-50 font-semibold">Email</label>
+                                            <InputText id="Email" type="email" class="!border !p-4 !text-primary-50 w-80"/>
+                                        </div>
+                                        <div class="inline-flex flex-col gap-2">
+                                            <label for="password" class="text-primary-50 font-semibold">Password</label>
+                                            <Password id="password" type="password" toggleMask :feedback="false" class="w-80 "/>
+                                        </div>
+                                        <div class="flex items-center gap-4 mt-4">
+                                            <Button label="Cancel" @click="closeCallback" text class="!p-4 w-full !text-primary-50 !border !border-[#6A9C89] hover:shadow-md hover:shadow-[#6A9C89] hover:!bg-[#CDF0EA]"></Button>
+                                            <Button label="Sign-In" @click="closeCallback" text class="!p-4 w-full !text-primary-50 !border !border-[#6A9C89] hover:shadow-md hover:shadow-[#6A9C89] hover:!bg-[#CDF0EA]"></Button>
+                                        </div>
+                                </div>
+                            </template>
+                        </Dialog>
                     </div>
-            </div>      
-        </div>
-        
+                          
+                </div>      
+            </div>
+        </div>      
 
 </template>
+
+<style scoped>
+.login-button{
+    color: black; /* Text color */
+    font-weight: bold; /* Font weight */  
+    width: 8.5rem; /* Width */
+    height: 3rem; /* Height */  
+    outline: 1px solid rgba(95, 190, 255, 1); /* Outline color */
+    box-shadow: 0 0 0 3px rgba(95, 190, 255, 0.5); /* Focus shadow */
+    
+}
+.login-button:hover {
+  box-shadow: 0 4px 6px -1px rgba(6, 182, 212, 0.5), 0 2px 4px -2px rgba(6, 182, 212, 0.5); /* Hover shadow */
+}
+
+:deep(.p-inputtext){
+    background-color: #CDF0EA;
+    border: 1px solid rgba(106, 156, 137,0.5);
+}
+:deep(.p-password-input){
+    width: 100%;
+    padding: 1rem;
+}
+</style>
 
